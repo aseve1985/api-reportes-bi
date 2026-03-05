7fedc9fce70b54f8a04db7cf742e64cf6f568eb8
Adrian Severino <severino.adrian@gmail.com>
2026-03-05 16:30:35 -0300

    Corrijo bug de fecha desembolso

--- app/config.py
+++ app/config.py
@@ -19,7 +19,7 @@ aws_access_key_id = os.getenv("aws_access_key_id")
 aws_secret_access_key = os.getenv("aws_secret_access_key")
 
 
-query = 'Select * from gold.ventas_arg WHERE fecha_creacion_loan BETWEEN %s AND %s'
+query = 'Select * from gold.ventas_arg WHERE fecha_desembolso BETWEEN %s AND %s'
 
 
 
